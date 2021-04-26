286e97359a5fe923050917f80852086adadcb35f
tonyg <aj.gianno@gmail.com>
2021-04-26 16:07:53 -0500

    AngularFire imports and environment config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBeBR1OfWh-2dp54uIkAjMhzqO4qZYrS7k',
+    authDomain: 'fir-practice-2dce1.firebaseapp.com',
+    projectId: 'fir-practice-2dce1',
+    storageBucket: 'fir-practice-2dce1.appspot.com',
+    messagingSenderId: '729397678119',
+    appId: '1:729397678119:web:c43ee0797b07cc16283e02',
+    measurementId: 'G-Y4XPK2912X',
+  },
 };
 
 /*
